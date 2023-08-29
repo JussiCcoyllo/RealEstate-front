1ad917cac7e8535a9047f08dcfcbfec02f5696fe
Jussi Ccoyllo <jussi.ccoyllo@gmail.com>
2023-08-29 19:36:21 +0200

    landing-page register added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppComponent } from './app.component';
 import { PropertyService } from './property.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './signup-page/login/login.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    LandingPageComponent,
+
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,
